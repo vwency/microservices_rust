8a630638d81e2371f56a4a909811274eab061edc
vwency <duckdodo666@gmail.com>
2025-03-23 20:56:00 +0600

    config instead of service & added gitattributes

--- hello_service/src/handlers/hello.rs
+++ hello_service/src/handlers/hello.rs
@@ -12,14 +12,16 @@ impl HelloService for MyHelloService {
         request: Request<HelloRequest>,
     ) -> Result<Response<HelloResponse>, Status> {
         let name = request.into_inner().name;
+        let name_lowercase = name.to_lowercase();
 
-        if name.to_lowercase().contains("hello") {
-            let reply = HelloResponse {
-                message: format!("Hello, {}!", name),
-            };
-            Ok(Response::new(reply))
-        } else {
-            Err(Status::invalid_argument("Message must contain 'hello'"))
+        if !name_lowercase.contains("hello") {
+            return Err(Status::invalid_argument("Message must contain 'hello'"));
         }
+
+        let reply = HelloResponse {
+            message: format!("Hello, {}!", name),
+        };
+
+        Ok(Response::new(reply))
     }
 }
--- hello_service/src/main.rs
+++ hello_service/src/main.rs
@@ -3,17 +3,25 @@ mod handlers;
 use handlers::hello::MyHelloService;
 use hello::hello_service_server::HelloServiceServer; 
 use tonic::transport::Server;
+use config::Config;
+use std::error::Error;
 
 pub mod hello {
     tonic::include_proto!("hello"); 
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:50051".parse()?;
+async fn main() -> Result<(), Box<dyn Error>> {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("configs/hello_service/config"))
+        .build()?;
+
+    let addr: std::net::SocketAddr = settings.get::<String>("server.address")?.parse()?;
+    let service_name = settings.get::<String>("server.name")?;
+    
     let hello_service = MyHelloService::default();
 
-    println!("HelloService running on {}", addr);
+    println!("{} running on {}", service_name, addr);
 
     Server::builder()
         .add_service(HelloServiceServer::new(hello_service))
